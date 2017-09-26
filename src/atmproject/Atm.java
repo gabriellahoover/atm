cdf20f0a0aa78ebb6f692d63610dd8a291597de4
ghoov <ghoov@10.0.126.121>
2017-09-26 13:02:21 -0400

    commit

--- src/atmproject/Atm.java
+++ src/atmproject/Atm.java
@@ -9,10 +9,40 @@ public class Atm {
 	
 	private int status;
 	
+	private int request;
+	private int requestedAmount;
+	
+	public boolean doAuthenticate() {
+		return true;
+	}
+	public void doHandleTransaction() {
+		if(this.request == 1) {
+			this.display("$"+ this.requestedAmount + " given");
+		}
+	}
+	
+	public void doHandleRequest() {
+		this.request = 1;
+		this.requestedAmount = 100;
+		
+	}
+	
+	public void doFinish() {
+		System.out.println("Thank you");
+	}
+	
+	public void display(String message) {
+		System.out.println(message);
+	}
 	public static void main(String[]args) {
 		Atm atm = new Atm();
 		/* authenticate*/
-		
+		boolean authenticated = atm.doAuthenticate();
+		if (authenticated) {
+		atm.doHandleRequest();
+		atm.doHandleTransaction();
+		atm.doFinish();
+		}
 	}
 
 
