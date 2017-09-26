57b2ec3b1ef252e310256872942db223b0635b86
ghoov <ghoov@10.0.126.121>
2017-09-26 12:39:08 -0400

    initial commit

---
+++ src/atmproject/Atm.java
@@ -0,0 +1,19 @@
+package atmproject;
+
+public class Atm {
+	private int accountNumber;
+	private String pin;
+	private double balance;
+	
+	private int amountOfMoneyInMachine;
+	
+	private int status;
+	
+	public static void main(String[]args) {
+		Atm atm = new Atm();
+		/* authenticate*/
+		
+	}
+
+
+}
---
+++ src/atmproject/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ghoov
+ *
+ */
+package atmproject;
